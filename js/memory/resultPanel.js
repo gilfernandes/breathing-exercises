172e05665a3b09370dd1f5143a4dd50711596c6c
Gil Fernandes <gil.fernandes@gmail.com>
2017-07-23 15:00:18 +0100

    Added scoring part

--- js/memory/resultPanel.js
+++ js/memory/resultPanel.js
@@ -1,12 +1,14 @@
 function ResultPanel() {
     createDiv("<div id='resultPanel'></div>");
-    this.display = false;
+    this.display = true;
 }
 
 ResultPanel.prototype.update = function(successCount, failCount) {
     if(this.display) {
         document.getElementById("resultPanel").style['display'] = 'block';
         document.getElementById("resultPanel").innerHTML = "";
-        createDiv(`<div>OK: ${successCount}</div><div>Fail: ${failCount}</div>`).parent("resultPanel");
+        const scoreInfo = `<div id="resultPanelGrid"><span class="successful">${successCount}</span><span class="sep">/</span><span class="failed">${failCount}</span></div>`;
+        const label = "<div id='yourScore'>Your Score</div>";
+        createDiv(scoreInfo + label).parent("resultPanel");
     }
 };
