53b6a30e7922016bac1f7b254912013f6eab1ea8
Gil Fernandes <gil.fernandes@gmail.com>
2017-07-31 20:19:48 +0100

    Moved the progress to separate canvas.

--- js/breathe/progress.js
+++ js/breathe/progress.js
@@ -1,24 +1,47 @@
-function Progress(missingTime, totalTime, totalWidth, progressbarGraphics) {
+function Progress(missingTime, totalTime, totalWidth, progressbarGraphics, progressHeight) {
     this.missingTime = missingTime;
     this.totalTime = totalTime;
     this.totalWidth = totalWidth;
-    this.progressHeight = 10;
     this.progressbarGraphics = progressbarGraphics;
+    this.progressHeight = progressHeight;
 }
+
+Progress.prototype.setProperties = function(missingTime, totalTime, totalWidth, progressbarGraphics) {
+    this.missingTime = missingTime;
+    this.totalTime = totalTime;
+    this.totalWidth = totalWidth;
+    this.progressbarGraphics = progressbarGraphics;
+};
 
 Progress.prototype.getProgressWidth = function () {
     return (this.missingTime * this.totalWidth) / this.totalTime;
 };
 
-Progress.prototype.setProgressbarGraphics = function (progressbarGraphics) {
-    this.progressbarGraphics = progressbarGraphics;
+Progress.prototype.fillProgress = function (canvas) {
+    canvas.background(60);
+    canvas.noStroke();
+    canvas.fill(255, 255, 255);
+    canvas.rect(0, 0, this.getProgressWidth(), this.progressHeight);
 };
 
-Progress.prototype.fillProgress = function (width) {
+let progressHeigth = function() {
+    return window.innerHeight * 0.02;
+};
 
-    this.progressbarGraphics.background(60);
-    this.progressbarGraphics.noStroke();
-    this.progressbarGraphics.fill(255, 255, 255);
-    this.progressbarGraphics.rect(0, 0, this.getProgressWidth(), this.progressHeight);
-    image(this.progressbarGraphics, -width * 0.5, height * 0.5 - this.progressHeight)
+let initProgress = function () {
+    const progressHeigth2 = progressHeigth();
+    progress = new Progress(standardBreathing.time, standardBreathing.time, window.innerWidth, null, progressHeigth2);
+    let canvas = new p5(initProgressCanvas);
+};
+
+let initProgressCanvas = function( p ) {
+
+    p.setup = function() {
+        p.createCanvas(window.innerWidth, progressHeigth());
+        p.background(255);
+    };
+
+    p.draw = function() {
+        progress.fillProgress(p);
+    };
 };
